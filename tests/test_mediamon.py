3a0d4af2c3aef12ee642342ad586e862ef5366a9
Christophe Lambin <christophe.lambin@gmail.com>
2020-06-18 22:51:37 +0200

    unit testing

--- tests/test_mediamon.py
+++ tests/test_mediamon.py
@@ -13,7 +13,22 @@ def test_initialise():
     assert len(scheduler.scheduled_items) == 3
     assert type(scheduler.scheduled_items[0].probe) is TransmissionProbe
     assert type(scheduler.scheduled_items[1].probe) is MonitorProbe
+    assert scheduler.scheduled_items[1].probe.app == MonitorProbe.App.sonarr
+    assert type(scheduler.scheduled_items[2].probe) is MonitorProbe
+    assert scheduler.scheduled_items[2].probe.app == MonitorProbe.App.radarr
+
+
+def test_missing_apikey():
+    config = argparse.Namespace(interval=0, port=8080,
+                                transmission='http://localhost:8080',
+                                sonarr='http://localhost:8081', sonarr_apikey='',
+                                radarr='http://localhost:8082', radarr_apikey='notreallyakey',
+                                once=True, stub=True, debug=True)
+    scheduler = initialise(config)
+    assert len(scheduler.scheduled_items) == 2
+    assert type(scheduler.scheduled_items[0].probe) is TransmissionProbe
     assert type(scheduler.scheduled_items[1].probe) is MonitorProbe
+    assert scheduler.scheduled_items[1].probe.app == MonitorProbe.App.radarr
 
 
 # def test_mediamon():
