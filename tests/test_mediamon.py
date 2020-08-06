838a0341e4a288846a2637e925aa1956a346a198
Christophe Lambin <christophe.lambin@gmail.com>
2020-08-06 21:37:01 +0200

    Move services to yml file (so we can put it in docker secrets)

--- tests/test_mediamon.py
+++ tests/test_mediamon.py
@@ -8,11 +8,12 @@ from src.plex import PlexServer
 def test_initialise():
     config = argparse.Namespace(interval=0, port=8080,
                                 once=True, stub=True, debug=True,
-                                services= {
+                                services={
                                     'transmission': {'host': '192.168.0.10:9091'},
                                     'sonarr': {'host': '192.168.0.10:8989', 'apikey': 'sonar-api-key'},
                                     'radarr': {'host': '192.168.0.10:7878', 'apikey': 'radar-api-key'},
-                                    'plex': {'username': 'email@example.com', 'password': 'some-password'}})
+                                    'plex': {'username': 'email@example.com', 'password': 'some-password'}
+                                })
 
     scheduler = initialise(config)
     assert len(scheduler.scheduled_items) == 4
@@ -23,4 +24,3 @@ def test_initialise():
     assert scheduler.scheduled_items[2].probe.app == MonitorProbe.App.radarr
     assert type(scheduler.scheduled_items[2].probe) is MonitorProbe
     assert type(scheduler.scheduled_items[3].probe) is PlexServer
-
