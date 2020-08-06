846e76ce1d9e0a543a68607d9970a4b7310dfc9a
Christophe Lambin <christophe.lambin@gmail.com>
2020-08-06 21:22:38 +0200

    Move services to yml file (so we can put it in docker secrets)

--- tests/test_mediamon.py
+++ tests/test_mediamon.py
@@ -2,47 +2,25 @@ import argparse
 from src.mediamon import initialise
 from src.transmission import TransmissionProbe
 from src.xxxarr import MonitorProbe
+from src.plex import PlexServer
 
 
 def test_initialise():
     config = argparse.Namespace(interval=0, port=8080,
-                                transmission='http://localhost:8080',
-                                sonarr='http://localhost:8081', sonarr_apikey='notreallyakey',
-                                radarr='http://localhost:8082', radarr_apikey='notreallyakey',
-                                plex_username='', plex_password='',
-                                once=True, stub=True, debug=True)
+                                once=True, stub=True, debug=True,
+                                services= {
+                                    'transmission': {'host': '192.168.0.10:9091'},
+                                    'sonarr': {'host': '192.168.0.10:8989', 'apikey': 'sonar-api-key'},
+                                    'radarr': {'host': '192.168.0.10:7878', 'apikey': 'radar-api-key'},
+                                    'plex': {'username': 'email@example.com', 'password': 'some-password'}})
+
     scheduler = initialise(config)
-    assert len(scheduler.scheduled_items) == 3
+    assert len(scheduler.scheduled_items) == 4
     assert type(scheduler.scheduled_items[0].probe) is TransmissionProbe
     assert type(scheduler.scheduled_items[1].probe) is MonitorProbe
     assert scheduler.scheduled_items[1].probe.app == MonitorProbe.App.sonarr
     assert type(scheduler.scheduled_items[2].probe) is MonitorProbe
     assert scheduler.scheduled_items[2].probe.app == MonitorProbe.App.radarr
+    assert type(scheduler.scheduled_items[2].probe) is MonitorProbe
+    assert type(scheduler.scheduled_items[3].probe) is PlexServer
 
-
-def test_missing_sonarr_apikey():
-    config = argparse.Namespace(interval=0, port=8080,
-                                transmission='http://localhost:8080',
-                                sonarr='http://localhost:8081', sonarr_apikey='',
-                                radarr='http://localhost:8082', radarr_apikey='notreallyakey',
-                                plex_username='', plex_password='',
-                                once=True, stub=True, debug=True)
-    scheduler = initialise(config)
-    assert len(scheduler.scheduled_items) == 2
-    assert type(scheduler.scheduled_items[0].probe) is TransmissionProbe
-    assert type(scheduler.scheduled_items[1].probe) is MonitorProbe
-    assert scheduler.scheduled_items[1].probe.app == MonitorProbe.App.radarr
-
-
-def test_missing_radarr_apikey():
-    config = argparse.Namespace(interval=0, port=8080,
-                                transmission='http://localhost:8080',
-                                sonarr='http://localhost:8081', sonarr_apikey='notreallyakey',
-                                radarr='http://localhost:8082', radarr_apikey='',
-                                plex_username='', plex_password='',
-                                once=True, stub=True, debug=True)
-    scheduler = initialise(config)
-    assert len(scheduler.scheduled_items) == 2
-    assert type(scheduler.scheduled_items[0].probe) is TransmissionProbe
-    assert type(scheduler.scheduled_items[1].probe) is MonitorProbe
-    assert scheduler.scheduled_items[1].probe.app == MonitorProbe.App.sonarr
