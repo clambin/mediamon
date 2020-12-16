5931c8f49863b4de0f0bcb160c7485c5dec5ead4
Christophe Lambin <christophe.lambin@gmail.com>
2020-12-16 23:20:13 +0100

    ported to golang

--- internal/transmission/apiclient.go
+++ internal/transmission/apiclient.go
@@ -3,6 +3,7 @@ package transmission
 import (
 	"bytes"
 	"errors"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 )
@@ -39,7 +40,11 @@ func (client *Client) Call(method string) ([]byte, error) {
 	if err == nil {
 		defer resp.Body.Close()
 
-		if resp.StatusCode == 200 {
+		if resp.StatusCode == 409 {
+			log.Warning("Session ID expired")
+			client.sessionID = resp.Header.Get("X-Transmission-Session-Id")
+		} else if resp.StatusCode == 200 {
+			client.sessionID = resp.Header.Get("X-Transmission-Session-Id")
 			return ioutil.ReadAll(resp.Body)
 		}
 		err = errors.New(resp.Status)
