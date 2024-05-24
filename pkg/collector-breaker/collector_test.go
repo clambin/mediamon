8bf1e275399200a0b22230862cdb100dd8862d17
Christophe Lambin <christophe.lambin@gmail.com>
2024-05-24 16:13:58 +0200

    refactor: circuit breaker test to reduce log noise

--- pkg/collector-breaker/collector_test.go
+++ pkg/collector-breaker/collector_test.go
@@ -3,60 +3,67 @@ package collector_breaker
 import (
 	"errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 	"log/slog"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestCBCollector(t *testing.T) {
-	c := collector{}
+	c := collector{metric: prometheus.NewDesc("foo", "", nil, nil)}
 	defaultConfiguration.OpenDuration = 500 * time.Millisecond
 	cbCollector := New("test", &c, slog.Default())
 
 	t.Run("circuit is closed: collection returns metrics", func(tt *testing.T) {
-		ch := make(chan prometheus.Metric)
-		go func() { cbCollector.Collect(ch); close(ch) }()
-		m := <-ch
-		assert.Equal(t, `Desc{fqName: "foo", help: "", constLabels: {}, variableLabels: {}}`, m.Desc().String())
+		assert.NoError(tt, testutil.CollectAndCompare(
+			cbCollector,
+			strings.NewReader(`
+# HELP foo 
+# TYPE foo counter
+foo 1
+`),
+			"foo",
+		))
 	})
 
 	t.Run("circuit is open: collection returns no metrics", func(tt *testing.T) {
 		c.err = errors.New("err")
-		ch := make(chan prometheus.Metric)
-		go func() { cbCollector.Collect(ch); close(ch) }()
-		for m := range ch {
-			assert.Contains(t, m.Desc().String(), `Desc{fqName: "circuit_breaker_`)
-		}
+		assert.Zero(tt, testutil.CollectAndCount(cbCollector, "foo"))
 	})
 
 	t.Run("collection works. circuit eventually closes again", func(t *testing.T) {
 		c.err = nil
 		assert.Eventually(t, func() bool {
-			ch := make(chan prometheus.Metric)
-			go func() { cbCollector.Collect(ch); close(ch) }()
-
-			for m := range ch {
-				if m.Desc().String() == `Desc{fqName: "foo", help: "", constLabels: {}, variableLabels: {}}` {
-					return true
-				}
-			}
-			return false
+			return nil == testutil.CollectAndCompare(
+				cbCollector,
+				strings.NewReader(`
+# HELP foo 
+# TYPE foo counter
+foo 1
+`),
+				"foo",
+			)
 		}, time.Second, 100*time.Millisecond)
 	})
 }
 
 var _ Collector = collector{}
 
-type collector struct{ err error }
+type collector struct {
+	metric *prometheus.Desc
+	err    error
+}
 
-func (c collector) Describe(_ chan<- *prometheus.Desc) {
+func (c collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.metric
 }
 
 func (c collector) CollectE(ch chan<- prometheus.Metric) error {
 	if c.err != nil {
 		return c.err
 	}
-	ch <- prometheus.MustNewConstMetric(prometheus.NewDesc("foo", "", nil, nil), prometheus.CounterValue, 1.0)
+	ch <- prometheus.MustNewConstMetric(c.metric, prometheus.CounterValue, 1.0)
 	return nil
 }
