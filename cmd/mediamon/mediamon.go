7f28de089ac891fd71e5400fc9f401b6fa662139
Christophe Lambin <christophe.lambin@gmail.com>
2025-06-10 11:33:25 +0200

    build: simplify

--- cmd/mediamon/mediamon.go
+++ cmd/mediamon/mediamon.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"net/url"
 	"os/signal"
 	"syscall"
 
 	"codeberg.org/clambin/go-common/charmer"
+	"github.com/clambin/mediamon/v2/internal/collectors/bandwidth"
+	"github.com/clambin/mediamon/v2/internal/collectors/connectivity"
+	"github.com/clambin/mediamon/v2/internal/collectors/plex"
+	"github.com/clambin/mediamon/v2/internal/collectors/prowlarr"
+	"github.com/clambin/mediamon/v2/internal/collectors/transmission"
+	"github.com/clambin/mediamon/v2/internal/collectors/xxxarr"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/cobra"
@@ -28,6 +36,23 @@ var (
 		PreRun: func(cmd *cobra.Command, args []string) {
 			charmer.SetTextLogger(cmd, viper.GetBool("debug"))
 		},
+	}
+
+	arguments = charmer.Arguments{
+		"debug":                         {Default: false},
+		"metrics.path":                  {Default: "/metrics"},
+		"metrics.addr":                  {Default: ":9090"},
+		"transmission.url":              {Default: ""},
+		"sonarr.url":                    {Default: ""},
+		"sonarr.apikey":                 {Default: ""},
+		"radarr.url":                    {Default: ""},
+		"radarr.apikey":                 {Default: ""},
+		"plex.url":                      {Default: ""},
+		"plex.username":                 {Default: ""},
+		"plex.password":                 {Default: ""},
+		"openvpn.connectivity.proxy":    {Default: ""},
+		"openvpn.connectivity.interval": {Default: "10s"},
+		"openvpn.bandwidth.filename":    {Default: ""},
 	}
 )
 
@@ -65,23 +90,6 @@ func Main(cmd *cobra.Command, _ []string) {
 	logger.Info("mediamon exiting")
 }
 
-var arguments = charmer.Arguments{
-	"debug":                         {Default: false},
-	"metrics.path":                  {Default: "/metrics"},
-	"metrics.addr":                  {Default: ":9090"},
-	"transmission.url":              {Default: ""},
-	"sonarr.url":                    {Default: ""},
-	"sonarr.apikey":                 {Default: ""},
-	"radarr.url":                    {Default: ""},
-	"radarr.apikey":                 {Default: ""},
-	"plex.url":                      {Default: ""},
-	"plex.username":                 {Default: ""},
-	"plex.password":                 {Default: ""},
-	"openvpn.connectivity.proxy":    {Default: ""},
-	"openvpn.connectivity.interval": {Default: "10s"},
-	"openvpn.bandwidth.filename":    {Default: ""},
-}
-
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.Flags().StringVar(&configFilename, "config", "", "Configuration file")
@@ -105,4 +113,96 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		slog.Error("failed to read config file", "err", err)
 	}
+}
+
+type constructor struct {
+	name string
+	make func(string, string, *viper.Viper, *slog.Logger) (prometheus.Collector, error)
+}
+
+var constructors = map[string]constructor{
+	"transmission.url": {
+		name: "transmission",
+		make: func(url, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return transmission.NewCollector(url, logger)
+		},
+	},
+	"sonarr.url": {
+		name: "sonarr",
+		make: func(url, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return xxxarr.NewSonarrCollector(url, v.GetString("sonarr.apikey"), logger)
+		},
+	},
+	"radarr.url": {
+		name: "radarr",
+		make: func(url, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return xxxarr.NewRadarrCollector(url, v.GetString("radarr.apikey"), logger)
+		},
+	},
+	"prowlarr.url": {
+		name: "prowlarr",
+		make: func(url, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return prowlarr.New(url, v.GetString("prowlarr.apikey"), logger)
+		},
+	},
+	"plex.url": {
+		name: "plex",
+		make: func(url, version string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return plex.NewCollector(
+				version,
+				url,
+				v.GetString("plex.username"),
+				v.GetString("plex.password"),
+				logger,
+			), nil
+		},
+	},
+	"openvpn.connectivity.proxy": {
+		name: "vpn connectivity",
+		make: func(url, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			proxy, err := parseProxy(url)
+			if err != nil {
+				return nil, fmt.Errorf("invalid proxy. connectivity won't be monitored: %w", err)
+			}
+			return connectivity.NewCollector(
+				proxy,
+				v.GetDuration("openvpn.connectivity.interval"),
+				logger,
+			), nil
+		},
+	},
+	"openvpn.bandwidth.filename": {
+		name: "vpn bandwidth",
+		make: func(target, _ string, v *viper.Viper, logger *slog.Logger) (prometheus.Collector, error) {
+			return bandwidth.NewCollector(target, logger), nil
+		},
+	},
+}
+
+func createCollectors(version string, v *viper.Viper, logger *slog.Logger) []prometheus.Collector {
+	collectors := make([]prometheus.Collector, 0, len(constructors))
+	for key, c := range constructors {
+		l := logger.With("collector", c.name)
+		if value := v.GetString(key); value != "" {
+			collector, err := c.make(value, version, v, l)
+			if err != nil {
+				l.Error("error creating collector", "err", err)
+				continue
+			}
+			collectors = append(collectors, collector)
+			l.Info("collector added", "source", value)
+		}
+	}
+	return collectors
+}
+
+func parseProxy(proxyURL string) (*url.URL, error) {
+	proxy, err := url.Parse(proxyURL)
+	if err != nil {
+		return nil, err
+	}
+	if proxy.Scheme == "" || proxy.Host == "" {
+		return nil, fmt.Errorf("missing scheme / host")
+	}
+	return proxy, nil
 }
