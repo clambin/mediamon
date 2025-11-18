2e6e2233025fdb709367ce7d9c55501f165488b5
Christophe Lambin <christophe.lambin@gmail.com>
2025-11-18 09:40:17 +0100

    cleanup

--- internal/collectors/connectivity/connectivity.go
+++ internal/collectors/connectivity/connectivity.go
@@ -24,26 +24,21 @@ type Collector struct {
 	connection *measurer.Cached[float64]
 }
 
-func NewCollector(httpClient *http.Client, interval time.Duration, logger *slog.Logger) prometheus.Collector {
+func NewCollector(httpClient *http.Client, interval time.Duration, _ *slog.Logger) prometheus.Collector {
 	const target = "https://clients3.google.com/generate_204"
 	return &Collector{
 		connection: &measurer.Cached[float64]{
 			Interval: interval,
 			Do: func(ctx context.Context) (float64, error) {
-				attrs := make([]any, 0, 3)
 				req, _ := http.NewRequestWithContext(ctx, http.MethodGet, target, nil)
 				resp, err := httpClient.Do(req)
-				attrs = append(attrs, slog.Any("err", err))
 				if err == nil {
 					_ = resp.Body.Close()
-					attrs = append(attrs, slog.Any("status", resp.StatusCode))
 				}
 				var up float64
 				if err == nil && resp.StatusCode == http.StatusNoContent {
 					up = 1
 				}
-				attrs = append(attrs, slog.Any("up", up))
-				logger.Debug("connectivity check", attrs...)
 				return up, nil
 			},
 		},
