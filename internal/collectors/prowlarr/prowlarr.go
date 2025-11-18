b85a47a4f635833fc7cd2120c0bde8b62cfaf745
Christophe Lambin <christophe.lambin@gmail.com>
2025-11-18 09:37:52 +0100

    cleanup

--- internal/collectors/prowlarr/prowlarr.go
+++ internal/collectors/prowlarr/prowlarr.go
@@ -15,6 +15,54 @@ import (
 
 const refreshInterval = 15 * time.Minute
 
+func newMetrics(url string) map[string]*prometheus.Desc {
+	constLabels := prometheus.Labels{"application": "prowlarr", "url": url}
+	return map[string]*prometheus.Desc{
+		"indexerResponseTime": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "indexer_response_time"),
+			"Average response time in seconds",
+			[]string{"indexer"},
+			constLabels,
+		),
+		"indexerQueryTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "indexer_query_total"),
+			"Total number of queries to this indexer",
+			[]string{"indexer"},
+			constLabels,
+		),
+		"indexerGrabTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "indexer_grab_total"),
+			"Total number of grabs from this indexer",
+			[]string{"indexer"},
+			constLabels,
+		),
+		"indexerFailedQueryTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "indexer_failed_query_total"),
+			"Total number of failed queries to this indexer",
+			[]string{"indexer"},
+			constLabels,
+		),
+		"indexerFailedGrabTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "indexer_failed_grab_total"),
+			"Total number of failed grabs from this indexer",
+			[]string{"indexer"},
+			constLabels,
+		),
+		"userAgentQueryTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "user_agent_query_total"),
+			"Total number of queries by user agent",
+			[]string{"user_agent"},
+			constLabels,
+		),
+		"userAgentGrabTotal": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "prowlarr", "user_agent_grab_total"),
+			"Total number of grabs by user agent",
+			[]string{"user_agent"},
+			constLabels,
+		),
+	}
+}
+
 type Collector struct {
 	metrics      map[string]*prometheus.Desc
 	logger       *slog.Logger
@@ -65,9 +113,7 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	}
 	for _, indexer := range *stats.Indexers {
 		name := *indexer.IndexerName
-		//c.logger.Debug("indexer found", "indexer", name, "queries", indexer.NumberOfQueries, "grabs", indexer.NumberOfGrabs)
-		responseTimeMsec := *indexer.AverageResponseTime
-		ch <- prometheus.MustNewConstMetric(c.metrics["indexerResponseTime"], prometheus.GaugeValue, float64(responseTimeMsec)/1000, name)
+		ch <- prometheus.MustNewConstMetric(c.metrics["indexerResponseTime"], prometheus.GaugeValue, float64(*indexer.AverageResponseTime)/1000, name)
 		ch <- prometheus.MustNewConstMetric(c.metrics["indexerQueryTotal"], prometheus.CounterValue, float64(*indexer.NumberOfQueries), name)
 		ch <- prometheus.MustNewConstMetric(c.metrics["indexerFailedQueryTotal"], prometheus.CounterValue, float64(*indexer.NumberOfFailedQueries), name)
 		ch <- prometheus.MustNewConstMetric(c.metrics["indexerGrabTotal"], prometheus.CounterValue, float64(*indexer.NumberOfGrabs), name)
@@ -75,7 +121,6 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	}
 	for _, userAgent := range *stats.UserAgents {
 		agent := *userAgent.UserAgent
-		//c.logger.Debug("user agent found", "agent", agent, "queries", userAgent.NumberOfQueries, "grabs", userAgent.NumberOfGrabs)
 		ch <- prometheus.MustNewConstMetric(c.metrics["userAgentQueryTotal"], prometheus.CounterValue, float64(*userAgent.NumberOfQueries), agent)
 		ch <- prometheus.MustNewConstMetric(c.metrics["userAgentGrabTotal"], prometheus.CounterValue, float64(*userAgent.NumberOfGrabs), agent)
 	}
--- internal/collectors/xxxarr/xxxarr.go
+++ internal/collectors/xxxarr/xxxarr.go
@@ -18,6 +18,63 @@ const (
 	calendarMeasureInterval = 15 * time.Minute
 )
 
+func createMetrics(application, url string) map[string]*prometheus.Desc {
+	constLabels := prometheus.Labels{
+		"application": application,
+		"url":         url,
+	}
+	return map[string]*prometheus.Desc{
+		"version": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "version"),
+			"Version info",
+			[]string{"version"},
+			constLabels,
+		),
+		"health": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "health"),
+			"Server health",
+			[]string{"type"},
+			constLabels,
+		),
+		"calendar": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "calendar"),
+			"Upcoming episodes / movies",
+			[]string{"title"},
+			constLabels,
+		),
+		"queued_count": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "queued_count"),
+			"Episodes / movies being downloaded",
+			nil,
+			constLabels,
+		),
+		"queued_total": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "queued_total_bytes"),
+			"Size of episode / movie being downloaded in bytes",
+			[]string{"title"},
+			constLabels,
+		),
+		"queued_downloaded": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "queued_downloaded_bytes"),
+			"Downloaded size of episode / movie being downloaded in bytes",
+			[]string{"title"},
+			constLabels,
+		),
+		"monitored": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "monitored_count"),
+			"Number of Monitored series / movies",
+			nil,
+			constLabels,
+		),
+		"unmonitored": prometheus.NewDesc(
+			prometheus.BuildFQName("mediamon", "xxxarr", "unmonitored_count"),
+			"Number of Unmonitored series / movies",
+			nil,
+			constLabels,
+		),
+	}
+}
+
 type QueuedItem struct {
 	Name            string
 	TotalBytes      int64
@@ -191,61 +248,4 @@ func (c *Collector) collectLibrary(ch chan<- prometheus.Metric) error {
 	ch <- prometheus.MustNewConstMetric(c.metrics["monitored"], prometheus.GaugeValue, float64(library.Monitored))
 	ch <- prometheus.MustNewConstMetric(c.metrics["unmonitored"], prometheus.GaugeValue, float64(library.Unmonitored))
 	return nil
-}
-
-func createMetrics(application, url string) map[string]*prometheus.Desc {
-	constLabels := prometheus.Labels{
-		"application": application,
-		"url":         url,
-	}
-	return map[string]*prometheus.Desc{
-		"version": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "version"),
-			"Version info",
-			[]string{"version"},
-			constLabels,
-		),
-		"health": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "health"),
-			"Server health",
-			[]string{"type"},
-			constLabels,
-		),
-		"calendar": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "calendar"),
-			"Upcoming episodes / movies",
-			[]string{"title"},
-			constLabels,
-		),
-		"queued_count": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "queued_count"),
-			"Episodes / movies being downloaded",
-			nil,
-			constLabels,
-		),
-		"queued_total": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "queued_total_bytes"),
-			"Size of episode / movie being downloaded in bytes",
-			[]string{"title"},
-			constLabels,
-		),
-		"queued_downloaded": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "queued_downloaded_bytes"),
-			"Downloaded size of episode / movie being downloaded in bytes",
-			[]string{"title"},
-			constLabels,
-		),
-		"monitored": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "monitored_count"),
-			"Number of Monitored series / movies",
-			nil,
-			constLabels,
-		),
-		"unmonitored": prometheus.NewDesc(
-			prometheus.BuildFQName("mediamon", "xxxarr", "unmonitored_count"),
-			"Number of Unmonitored series / movies",
-			nil,
-			constLabels,
-		),
-	}
 }
